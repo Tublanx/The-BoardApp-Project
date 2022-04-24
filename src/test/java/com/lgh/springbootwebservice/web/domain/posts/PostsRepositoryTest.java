c8c88cf8ea1fab35d0caee729f397a49b2ce9e0f
Tublanx <rlgus03453@naver.com>
2022-04-24 22:19:13 +0900

    constructing insert/update query test code

---
+++ src/test/java/com/lgh/springbootwebservice/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.lgh.springbootwebservice.web.domain.posts;
+
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save() {
+        // given
+        String title = "test";
+        String content = "It is test";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("Lee Gi Hyeon").build()); // insert/update query to posts table
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
