0f8141e411f5dceb333860da1a41c2e9a274b035
Tublanx <rlgus03453@naver.com>
2022-04-24 22:06:29 +0900

    create posts table

---
+++ src/main/java/com/lgh/springbootwebservice/web/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.lgh.springbootwebservice.web.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 추가 (매개변수 x)
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
