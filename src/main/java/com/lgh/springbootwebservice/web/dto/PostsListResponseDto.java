c1d3afef3988486b5103d1ad62d58fae3a5ac69b
Tublanx <rlgus03453@naver.com>
2022-05-05 23:43:20 +0900

    update PostsListResponseDto

--- src/main/java/com/lgh/springbootwebservice/web/dto/PostsListResponseDto.java
+++ src/main/java/com/lgh/springbootwebservice/web/dto/PostsListResponseDto.java
@@ -3,18 +3,20 @@ package com.lgh.springbootwebservice.web.dto;
 import com.lgh.springbootwebservice.web.domain.posts.Posts;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostsListResponseDto {
 
     private Long id;
     private String title;
     private String author;
-    private String content;
+    private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.content = entity.getContent();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
