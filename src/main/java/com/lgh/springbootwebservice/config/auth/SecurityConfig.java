93c9b17d0d94609b8406ff5988d6dd42c2263f0d
Tublanx <rlgus03453@naver.com>
2022-05-07 19:55:38 +0900

    add SecurityConfig

---
+++ src/main/java/com/lgh/springbootwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.lgh.springbootwebservice.config.auth;
+
+import com.lgh.springbootwebservice.web.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests() // URL 별 권한 관리를 설정하는 옵션의 시작점
+                .antMatchers("/", "/css/**", "/images/**", "/js/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
